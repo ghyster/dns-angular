acc3b97e951c4ddc7d89c5858faa08a408bb641e
ghyster <nicolas.legras@gmail.com>
2017-09-19 09:59:17 +0000

    Apply fixes from StyleCI

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,54 +1,55 @@
-<?php namespace App\Http\Middleware;
+<?php
+
+namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Contracts\Auth\Guard;
 
-class Authenticate {
+class Authenticate
+{
+    /**
+     * The Guard implementation.
+     *
+     * @var Guard
+     */
+    protected $auth;
 
-	/**
-	 * The Guard implementation.
-	 *
-	 * @var Guard
-	 */
-	protected $auth;
+    /**
+     * Create a new filter instance.
+     *
+     * @param Guard $auth
+     *
+     * @return void
+     */
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
 
-	/**
-	 * Create a new filter instance.
-	 *
-	 * @param  Guard  $auth
-	 * @return void
-	 */
-	public function __construct(Guard $auth)
-	{
-		$this->auth = $auth;
-	}
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($this->auth->guest()) {
+            $authtype = \Config::get('app.authtype');
 
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next)
-	{
-		
-		if ($this->auth->guest()){
-			$authtype=\Config::get('app.authtype');
+            if ($request->ajax()) {
+                return response('Unauthorized.', 401);
+            } elseif ($authtype == 'google') {
+                return redirect()->guest('login/google');
+            } elseif ($authtype == 'saml') {
+                return redirect()->guest('saml2/login');
+            } else {
+                return response('Unauthorized.', 401);
+            }
+        }
 
-			if ($request->ajax()){
-				return response('Unauthorized.', 401);
-			}else if ($authtype == "google") {
-				return redirect()->guest('login/google');
-			}else if ($authtype == "saml"){
-				return redirect()->guest('saml2/login');
-			}else{
-				return response('Unauthorized.', 401);
-			}
-
-		}
-
-		return $next($request);
-	}
-
+        return $next($request);
+    }
 }
